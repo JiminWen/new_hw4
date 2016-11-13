309976dd740aef955d27de41903df2430413e8e7
Jimin Wen <jiminwen@tamu.edu>
2016-11-13 23:40:54 +0000

    can run

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  resources :movies
+  # map '/' to be a redirect to '/movies'
+  root :to => redirect('/movies')
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
