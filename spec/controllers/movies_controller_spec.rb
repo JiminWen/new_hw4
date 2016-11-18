b6ed6c3b837000dd8f22e0d2301049be06d9bb15
Jimin Wen <jiminwen@tamu.edu>
2016-11-18 23:06:58 +0000

    90%

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,12 +2,34 @@ require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
      before(:each) do
-      @movie = mock(Movie, :title => 'alien', :id => '1',:director => 'James')
-      @hello.stub!(:say).and_return("hello world")
+     @movie = double(Movie, :title => 'alien', :id => '1',:director => 'James')
+      allow(Movie).to receive(:find).with('1').and_return(@movie)
     end
-    #describe 'same director' do
-     # @fake=@movie
-     # Movie.should_receive(:same_directors).with(:James).and_return(@fake)
-    #  Movie.same_directors(:James)
-   # end
+    describe 'destroy' do
+     it 'call method' do 
+       # delete :destroy,{:id => '1'}
+        expect(@movie).to receive(:destroy)
+        delete :destroy, :id => '1'
+     end
+     it 'redirect_to' do
+        allow(@movie).to receive(:destroy)
+         delete :destroy, :id => '1'
+        expect(response).to redirect_to(movies_path) 
+     end
+    end 
+    describe 'create' do  
+     it 'create redirect_to' do
+         Movie.any_instance.stub(:valid?).and_return(true)
+         post :create, :movie => {:title => 'alien', :director => 'James',:release_date=>" ",:rating=>" ",:description=>" "}
+         assigns[:movie].should_not be_new_record
+         flash[:notice].should_not be_blank
+         response.should redirect_to(movies_path)
+      
+     end 
+   
+    end 
+    describe 'edit' do
+     it 
+    end 
+   
 end
